efad20e16ab6da5f8554915eb6848f19d1ad17d2
GlebKolyano <glebkolyano38@gmail.com>
2022-05-22 12:55:43 +0300

    solve the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const { stdin: input, stdout: output, exit, stdout } = require('process');
+const path = require('path');
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({ input, output });
+
+addText();
+
+function addText() {
+  let newText;
+  fs.readFile(path.join(__dirname, 'text.txt'), (err, data) => {
+    let oldText = '';
+    if (err) init();
+    if (data) oldText = data.toString();
+    newText = oldText;
+  });
+
+  rl.question('Привет! Введите текст для записи:  ', (str) => {
+    if (str.trim() === 'exit') return exit();
+    newText += ` ${str}`;
+    fs.writeFile(path.join(__dirname, 'text.txt'), newText, (err) => {
+      if (err) return console.error(err.message); 
+    });
+    console.log('\nЗаметка создана\n');
+    return addText();
+  });
+}
+
+function init() {
+  fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+    if (err) return console.error(err.message);
+  });
+}
+
+process.on('exit', () => stdout.write('\nЗапись окончена!'));
