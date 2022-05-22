25a707a72652d32108aa056b08a702809767ce50
GlebKolyano <88821881+GlebKolyano@users.noreply.github.com>
2022-05-22 13:16:02 +0300

    Update index.js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,14 +8,14 @@ readFiles();
 async function readFiles() {
   try {
     const files = await readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
-
+    console.log('\x1b[33m%s\x1b[0m', 'Если значения не такие как у вас, обратите внимание, что конвертация в kb!');
     for (const file of files)
       if (file.isFile()) {
         const name = file.name.toString().split('.')[0];
         const extension = path.extname(file.name).substring(1);
         const size = await fs.stat(path.join(__dirname, 'secret-folder', file.name));
 
-        const fileSizeInKb = (size.size * 0.00098).toFixed(3);
+        const fileSizeInKb = (size.size * 0.00098);
         const str = '```' + `${name} - ${extension} - ${fileSizeInKb}` + 'kb```';
         console.log(str);
       }
